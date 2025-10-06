d212880955ae3188361a01e262c28e7b143a73aa
Dominick <dominickfrench.oe@gmail.com>
2025-10-06 13:59:19 -0500

    Third Commit

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class FamousQuotes {
     public static void main(String[] args) {
-        String[] favQuotes = {"no idea" , "huh, what the" , "my head hurts"};
-
+        String[] favQuotes = {"No idea." , "Huh, what the?" , "My head hurts."};
         Scanner myScanner = new Scanner(System.in);
 
         System.out.println("Enter a number between 1 and 3: ");
-        String input = myScanner.nextLine();
-        int i = Integer.parseInt(input);
-        i--;
-        System.out.println(favQuotes[i]);
+
+        try {
+            int input = Integer.parseInt(myScanner.nextLine());
+            input--;
+            System.out.println(favQuotes[input]);
+        } catch (Exception e) {
+            System.out.println("Number entered is not between 1 and 3.");
+        }
 
     }
 }
