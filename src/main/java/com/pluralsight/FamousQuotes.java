6597d558a4b5352f40c78e6e519420eed55a8b1f
Dominick <dominickfrench.oe@gmail.com>
2025-10-06 13:33:21 -0500

    Second Commit

--- src/main/java/com/pluralsight/FamousQuotes.java
+++ src/main/java/com/pluralsight/FamousQuotes.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FamousQuotes {
+    public static void main(String[] args) {
+        String[] favQuotes = {"no idea" , "huh, what the" , "my head hurts"};
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter a number between 1 and 3: ");
+        String input = myScanner.nextLine();
+        int i = Integer.parseInt(input);
+        i--;
+        System.out.println(favQuotes[i]);
+
+    }
 }
